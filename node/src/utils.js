6335c47ad679bd78cf31ff8f54a1019a61fc80ff
RadamantiumX <eduardo2502@gmail.com>
2023-10-31 23:39:35 -0300

    git changes minor

--- node/src/utils.js
+++
@@ -1,4 +0,0 @@
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url)
-export const readJSONUsers = (path) => require('../users.json') 
-export const readJSONNotes = (path) => require('../notes.json')
